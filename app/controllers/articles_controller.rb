f3d0990dbd19d3081006120c187e9a7ccbcebfe2
usamajutt5761 <usamajutt5761@gmail.com>
2022-03-18 15:14:21 +0500

    whole  CRUD done

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -13,13 +13,34 @@ class ArticlesController < ApplicationController
  end
 
  def create
-   @article = Article.new(article_params)
+    @article = Article.new(article_params)
 
-  if @article.save
-    redirect_to articles_path
-  else
-    render :new, status: :unprocessable_entity
-  end
+    if @article.save
+     redirect_to articles_path
+    else
+     render :new, status: :unprocessable_entity
+    end
+ end
+
+ def edit
+    @article = Article.find(params[:id])
+ end
+
+ def update
+    @article = Article.find(params[:id])
+
+    if @article.update(article_params)
+      redirect_to @article
+    else
+      render :edit, status: :unprocessable_entity
+    end
+ end
+
+ def destroy
+    @article = Article.find(params[:id])
+    @article.destroy
+
+    redirect_to articles_path, status: :see_other
  end
 
  private
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,9 @@
 Rails.application.routes.draw do
   resources :entries
+  resources :articles
+  get "test", to: 'test#index'
+  get "about", to: 'test#about'
+
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
